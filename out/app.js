c36b9d096642610fa73242062d3229337b2cb7e1
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-06-26 13:04:33 +0300

    create ts

---
+++ out/app.js
@@ -0,0 +1,4 @@
+console.log('Hello World');
+console.log('Seda Diriker Seda');
+let x = 10;
+let y = 5;
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+console.log('Hello World')
+
+let x = 10
+let y = 5
+// yorumlar kapalı, bu gözükmeyecek
